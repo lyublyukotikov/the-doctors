fe28672d523afca26a6b9aad6c1d0e59258ee99c
Olegatron1 <120267498+Olegatron1@users.noreply.github.com>
2024-10-31 02:28:12 +0300

    Revert "Backend"

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -46,14 +45,4 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
-
-	public function doctors(): HasMany
-	{
-		return $this->hasMany(Doctor::class);
-	}
-
-	public function clients(): HasMany
-	{
-		return $this->hasMany(Client::class);
-	}
 }
